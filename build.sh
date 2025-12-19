77b6cec8f081ac8442882fedb4e78c99aec1c817
Arka Karmoker <arkakarmoker1234@gmail.com>
2025-12-20 03:56:37 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,12 +10,6 @@ python manage.py collectstatic --no-input
 # Apply database migrations
 python manage.py migrate
 
-# -------------------------------------------------------------------------
-# AUTO-CREATE SUPERUSER (Only for SQLite Demo)
-# -------------------------------------------------------------------------
-# This creates a user: 
-# Username: admin
-# Password: password123
-# Email: admin@example.com
-echo "Creating default superuser..."
+# Create Superuser
+echo "Creating superuser..."
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'password123')"
